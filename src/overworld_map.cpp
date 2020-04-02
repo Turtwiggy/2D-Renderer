4a6d6b4272c5754816f1eb95f7d9199db979f1eb
James <Icedshot@gmail.com>
2020-04-02 10:06:09 +0100

    create two armies

--- src/overworld_map.cpp
+++ src/overworld_map.cpp
@@ -270,20 +270,14 @@ entt::entity create_overworld(entt::registry& registry, random_state& rng, vec2i
     return res;
 }
 
-void debug_overworld(entt::registry& registry, entt::entity en, random_state& rng)
+entt::entity create_dummy_army_at(entt::registry& registry, random_state& rng, vec2i pos, int team_id)
 {
-    int root_id = 0;
-
-    tilemap& tmap = registry.get<tilemap>(en);
-
-    vec2i half = tmap.dim/2;
-
     world_transform transform;
-    transform.position = vec2f{half.x(), half.y()} * TILE_PIX + vec2f{TILE_PIX/2, TILE_PIX/2};
+    transform.position = vec2f{pos.x(), pos.y()} * TILE_PIX + vec2f{TILE_PIX/2, TILE_PIX/2};
 
     team base_team;
     base_team.type = team::NUMERIC;
-    base_team.t = root_id;
+    base_team.t = team_id;
 
     entt::entity army = create_unit_group(registry, base_team, get_sprite_handle_of(rng, tiles::SOLDIER_SPEAR), transform);
 
@@ -292,7 +286,7 @@ void debug_overworld(entt::registry& registry, entt::entity en, random_state& rn
     for(int i=0; i < unit_count; i++)
     {
         world_transform trans;
-        trans.position = vec2f{half.x(), half.y()} * TILE_PIX + vec2f{TILE_PIX/2, TILE_PIX/2};
+        trans.position = vec2f{pos.x(), pos.y()} * TILE_PIX + vec2f{TILE_PIX/2, TILE_PIX/2};
 
         damageable damage;
 
@@ -303,5 +297,18 @@ void debug_overworld(entt::registry& registry, entt::entity en, random_state& rn
         ugroup.entities.push_back(en);
     }
 
-    tmap.add(army, half);
+    return army;
+}
+
+void debug_overworld(entt::registry& registry, entt::entity en, random_state& rng)
+{
+    tilemap& tmap = registry.get<tilemap>(en);
+
+    vec2i half = tmap.dim/2;
+
+    entt::entity army1 = create_dummy_army_at(registry, rng, half, 0);
+    entt::entity army2 = create_dummy_army_at(registry, rng, {half.x()+1, half.y()}, 0);
+
+    tmap.add(army1, half);
+    tmap.add(army2, {half.x()+1, half.y()});
 }
