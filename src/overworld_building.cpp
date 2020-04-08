e18efcf2d2c84a728651d3a7cc6957eddde2c14c
James <Icedshot@gmail.com>
2020-04-08 22:31:39 +0100

    beginning of basic ck2 style build system

--- src/overworld_building.cpp
+++ src/overworld_building.cpp
@@ -5,6 +5,52 @@
 #include "sprite_renderer.hpp"
 #include <imgui/imgui.h>
 
+template<typename T>
+void def(T& in, const building_feature& f)
+{
+    in[(int)f.t] = f;
+}
+
+bool building_feature::can_build_on(building_tag::categories cat)
+{
+    for(auto& i : buildable_on)
+    {
+        if(i == cat)
+            return true;
+    }
+
+    return false;
+}
+
+building_feature get_feature_info(building_feature::type type)
+{
+    std::array<building_feature, building_feature::COUNT> var;
+
+    def(var, {building_feature::WALLS, 0, 2, {building_tag::VILLAGE, building_tag::CASTLE, building_tag::TOWN, building_tag::CHURCH}});
+    def(var, {building_feature::BARRACKS, 0, 1, {building_tag::VILLAGE, building_tag::CASTLE, building_tag::TOWN}});
+    def(var, {building_feature::STABLE, 0, 2, {building_tag::VILLAGE, building_tag::CASTLE, building_tag::TOWN}});
+    def(var, {building_feature::MINE, 0, 3, {building_tag::VILLAGE, building_tag::TOWN}});
+
+    return var[(int)type];
+}
+
+std::vector<building_feature> get_buildable_for(building_tag::categories cat)
+{
+    std::vector<building_feature> vec;
+
+    for(int i=0; i < (int)building_feature::type::COUNT; i++)
+    {
+        building_feature next = get_feature_info((building_feature::type)i);
+
+        if(!next.can_build_on(cat))
+            continue;
+
+        vec.push_back(next);
+    }
+
+    return vec;
+}
+
 entt::entity create_overworld_building(entt::registry& registry, const sprite_handle& handle, const world_transform& transform)
 {
     entt::entity res = registry.create();
--- src/overworld_building.hpp
+++ src/overworld_building.hpp
@@ -2,14 +2,51 @@
 #define OVERWORLD_BUILDING_HPP_INCLUDED
 
 #include <entt/entt.hpp>
+#include <vector>
 
 struct sprite_handle;
 struct world_transform;
 
+struct building_feature;
+
 struct building_tag
 {
+    enum categories
+    {
+        VILLAGE,
+        CASTLE,
+        TOWN,
+        CHURCH,
+    };
 
+    categories cat = categories::VILLAGE;
+    std::vector<building_feature> built;
+
+    //std::vector<building_feature> get_buildable();
 };
+
+//Not every building category will be able to build all features
+struct building_feature
+{
+    enum type
+    {
+        WALLS,
+        BARRACKS,
+        STABLE,
+        MINE,
+        COUNT
+    };
+
+    type t = type::COUNT;
+    int level = 0;
+    int max_level = 0;
+    std::vector<building_tag::categories> buildable_on;
+
+    bool can_build_on(building_tag::categories cat);
+};
+
+building_feature get_feature_info(building_feature::type type);
+std::vector<building_feature> get_buildable_for(building_tag::categories cat);
 
 entt::entity create_overworld_building(entt::registry& registry, const sprite_handle& handle, const world_transform& transform);
 
