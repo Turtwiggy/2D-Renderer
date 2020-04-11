67efa57537236ec526126150f0f1634bad263080
James <Icedshot@gmail.com>
2020-04-11 02:32:50 +0100

    more serialisation

--- src/serialisables.hpp
+++ src/serialisables.hpp
@@ -3,4 +3,7 @@
 #include <networking/serialisable_fwd.hpp>
 
 DECLARE_SERIALISE_FUNCTION(tilemap);
+DECLARE_SERIALISE_FUNCTION(sprite_handle);
+DECLARE_SERIALISE_FUNCTION(world_transform);
+DECLARE_SERIALISE_FUNCTION(render_descriptor);
 
